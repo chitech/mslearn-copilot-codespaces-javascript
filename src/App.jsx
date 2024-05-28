1391815626bddce6ce4cc788f761cba8b26ce3fc
Christine Brown Clayton <chicagotechpro@gmail.com>
2024-05-28 20:43:25 +0000

    Customized portfolio with headshot and updated content

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Christine Brown Clayton",
+  title: "Software Developer/AI Cloud Engineer",
+  email: "chrisitne@example.com",
+  gitHub: "chitech",
+  instagram: "unieros",
+  linkedIn: "christinebrownclayton",
   medium: "",
-  twitter: "microsoft",
+  twitter: "chitechpro",
   youTube: "Code",
 };
 
